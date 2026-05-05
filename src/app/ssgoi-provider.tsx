df144e60d7594945841eda4abad04ee0d3db0469
Ryu Tae Hyun <ryu200112@gmail.com>
2026-05-05 16:08:22 +0900

    fix: ssgoi 재적용

--- src/app/ssgoi-provider.tsx
+++ src/app/ssgoi-provider.tsx
@@ -3,17 +3,17 @@
 import type { ReactNode } from "react";
 import { usePathname } from "next/navigation";
 import { Ssgoi } from "@ssgoi/react";
-import { blind, fade } from "@ssgoi/react/view-transitions";
+import { fade } from "@ssgoi/react/view-transitions";
 
 const ssgoiConfig = {
-  // defaultTransition: fade({
-  //   physics: {
-  //     spring: {
-  //       stiffness: 500, // 스프링 강도: 높을수록 더 빠르고 강하게 움직임
-  //       damping: 40, // 감쇠: 높을수록 덜 튀고 더 빨리 안정됨
-  //     },
-  //   },
-  // }),
+  defaultTransition: fade({
+    physics: {
+      spring: {
+        stiffness: 700, // 스프링 강도: 높을수록 더 빠르고 강하게 움직임
+        damping: 30, // 감쇠: 높을수록 덜 튀고 더 빨리 안정됨
+      },
+    },
+  }),
 };
 
 export default function SsgoiProvider({ children }: { children: ReactNode }) {
